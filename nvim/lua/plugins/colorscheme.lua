968e80bf73c3bd478f2cb1794fe8c0442842764e
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-03-01 23:42:33 +0000

    switch to tokyodark colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,56 @@
 return {
+	-- {
+	-- 	-- "eggfriedrice24/eggfriedrice.nvim",
+	-- 	dir = "/home/eggfriedrice/p/eggfriedrice.nvim",
+	-- 	priority = 1000,
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 		require("eggfriedrice").setup({
+	-- 			transparent = true,
+	-- 		})
+	--
+	-- 		vim.cmd.colorscheme("eggfriedrice")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"olimorris/onedarkpro.nvim",
+	-- 	priority = 1000,
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 		require("onedarkpro").setup({
+	-- 			options = {
+	-- 				transparency = true,
+	-- 			},
+	-- 		})
+	--
+	-- 		vim.cmd.colorscheme("onedark")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	priority = 1000,
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 		require("rose-pine").setup({
+	-- 			styles = {
+	-- 				transparency = true,
+	-- 			},
+	-- 		})
+	--
+	-- 		vim.cmd.colorscheme("rose-pine")
+	-- 	end,
+	-- },
 	{
-		-- "eggfriedrice24/eggfriedrice.nvim",
-		dir = "/home/eggfriedrice/p/eggfriedrice.nvim",
+		"tiagovla/tokyodark.nvim",
 		priority = 1000,
 		lazy = false,
 		config = function()
-			require("eggfriedrice").setup({
-				transparent = true,
+			require("tokyodark").setup({
+				transparent_background = true,
 			})
 
-			vim.cmd.colorscheme("eggfriedrice")
+			vim.cmd.colorscheme("tokyodark")
 		end,
 	},
 }
