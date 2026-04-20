6b3a0238b9f752514d1eec1573cb321c00dc9ff5
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-04-20 13:50:46 +0100

    rename swww to awww following arch package replacement

--- hypr/randwall.sh
+++ hypr/randwall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swww img `find $wallpaper_path -type f | shuf -n 1` --transition-type any --transition-fps 60
+awww img `find $wallpaper_path -type f | shuf -n 1` --transition-type any --transition-fps 60
