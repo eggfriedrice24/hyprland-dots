7c3e9a7374863f880987d08a8d8d42e7e67d6887
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-08-12 12:57:02 +0200

    configure hdmi second monitor left of main on desktop

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -8,6 +8,7 @@ require("keybinds")
 
 -- Monitors
 hl.monitor({ output = "DP-3", mode = "2560x1440@240", position = "0x0", scale = 1 })
+hl.monitor({ output = "HDMI-A-1", mode = "2560x1440@60", position = "-2560x0", scale = 1 }) -- HP P27q, left of main
 hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = 1 })
 -- hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
 
